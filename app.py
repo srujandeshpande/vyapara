168d55b247d7a2cb8993545c666070fa7353728c
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-30 14:09:17 +0530

    Added login, new user, new product and new sale

--- app.py
+++ app.py
@@ -18,30 +18,70 @@ db = pymongo.database.Database(mongo, 'vyapara')
 def test():
     return "Works"
 
-@app.route('/api/add_product', methods=['POST'])
-def add_product():
-    inputData = request.form
+
+@app.route('/api/new_buyer', methods=['POST'])
+def new_buyer():
+    inputData = request.json
+    Buyer_Data = pymongo.collection.Collection(db, 'Buyer_Data')
+    for i in json.loads(dumps(Buyer_Data.find())):
+        if i['email'] == inputData['email']:
+            return Response(status=403)
+    Buyer_Data.insert_one({'email':inputData['email'],'password':inputData['password']});
+    return Response(status=200)
+
+
+@app.route('/api/new_seller', methods=['POST'])
+def new_seller():
+    inputData = request.json
+    Seller_Data = pymongo.collection.Collection(db, 'Seller_Data')
+    for i in json.loads(dumps(Seller_Data.find())):
+        if i['email'] == inputData['email']:
+            return Response(status=403)
+    Seller_Data.insert_one({'email':inputData['email'],'password':inputData['password']});
+    return Response(status=200)
+
+
+@app.route('/api/login_buyer', methods=['POST'])
+def login_buyer():
+    inputData = request.json
+    Buyer_Data = pymongo.collection.Collection(db, 'Buyer_Data')
+    for i in json.loads(dumps(Buyer_Data.find())):
+        if i['email'] == inputData['email'] and i['password'] == inputData['password']:
+            return Response(status=200)
+        else:
+            return Response(status=403)
+
+
+@app.route('/api/login_seller', methods=['POST'])
+def login_seller():
+    inputData = request.json
+    Seller_Data = pymongo.collection.Collection(db, 'Seller_Data')
+    for i in json.loads(dumps(Seller_Data.find())):
+        if i['email'] == inputData['email'] and i['password'] == inputData['password']:
+            return Response(status=200)
+        else:
+            return Response(status=403)
+
+
+@app.route('/api/add_new_product', methods=['POST'])
+def add_new_product():
+    inputData = request.json
     Product_Data = pymongo.collection.Collection(db, 'Product_Data')
     Seller_Data = pymongo.collection.Collection(db, 'Seller_Data')
     for i in json.loads(dumps(Seller_Data.find())):
-        if i['phone_number'] == inputData['phone_number'] and i['password'] == inputData['password']:
-            if(i['ema_role'] == ""):
-                flash("Invalid input")
-                return redirect(url_for('ema_loginscreen'))
-            else:
-                session['phone_number'] = inputData['phone_number']
-                session['ema_role'] = i['ema_role']
-                return redirect(url_for('ema_dashboard'))
-    flash("Please enter valid credentials")
-    return redirect(url_for('ema_loginscreen'))
+        if i['email'] == inputData['email']:
+            Product_Data.insert_one({'seller':inputData['email'],'name':inputData['name'],'price':inputData['price'],'description':inputData['description']})
+            return Response(status=200)
+    return Response(status=403)
 
 
-@app.route('/tests/build_test')
-def build_test():
-	return "Passed"
-
-
-#Create Userpage
-@app.route('/user/<phone_number>')
-def user_page(phone_number):
-	return render_template('user_page.html')
+@app.route('/api/add_new_sale', methods=['POST'])
+def add_new_sale():
+    inputData = request.json
+    Product_Data = pymongo.collection.Collection(db, 'Product_Data')
+    Buyer_Data = pymongo.collection.Collection(db, 'Buyer_Data')
+    for i in json.loads(dumps(Buyer_Data.find())):
+        if i['email'] == inputData['email']:
+            Sales_Data.insert_one({'product':inputData['product_id'],'buyer':inputData['email'],'date':inputData['date']})
+            return Response(status=200)
+    return Response(status=403)
